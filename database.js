0f459a06f6c1e348633c4f92808aec7aa745a3e4
saiprasanth03 <ssaiprasanth333@example.com>
2026-03-28 18:16:40 +0530

    fix glibc issue

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 const path = require('path');
 
 const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'));
